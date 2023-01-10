269bc8a24685916e3d75c90c102f5235ed15ccdc
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-01-10 19:21:16 +0200

    2.31

--- OR just a one file/one file unit coverter.c
+++ OR just a one file/one file unit coverter.c
@@ -764,6 +764,77 @@ void volume_coverter(void)
     }
     system("cls");
 }
+#include "force.h"
+
+void force_coverter(void)
+{
+    system("cls");
+
+    while(1)
+    {
+        float force;
+        short unit;
+        char end_c;
+        printf ("  Enter the unit of the force : \n");
+        printf ("  ---------------\n");
+        printf ("  | 1 |- Newton  |\n" );
+        printf ("  | 2 |- Pound.  |\n" );
+        printf ("  ---------------\n");
+        fflush(stdin);
+        printf("\n>> ");
+        scanf("%d",&unit);
+        if(unit==1)
+        {
+            printf("  Enter The Force Value :\n");
+            printf("\n>> ");
+            scanf("%f",&force);
+            if(force>=0)
+            {
+                force=force/4.448222;
+                printf("  The force = %f Ib",force);
+            }
+            else
+            {
+                printf("\n  Worng Entery  \n");
+            }
+
+        }
+        else if(unit==2)
+        {
+            printf("  Enter The Force Value :\n");
+            printf("\n>> ");
+            scanf("%f",&force);
+            if(force>=0)
+            {
+                force=force*4.448222;
+                printf("  The force = %f N\n",force);
+            }
+            else
+            {
+                printf("\n  Worng Entery  \n");
+            }
+
+
+        }
+        else
+        {
+            printf("\n  Worng Entery  \n");
+        }
+        printf("\n press Esc to back to main menu or any thing to continue\n");
+        printf("\n>> ");
+        end_c = getch();
+        system("cls");
+
+        if (end_c == 27)
+        {
+            main();
+            break;
+        }
+    }
+    system("cls");
+
+}
+
 
 void temp_converter(void)
 {
